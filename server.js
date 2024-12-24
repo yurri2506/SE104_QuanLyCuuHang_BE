15063a43c19c999ae8798cbce9d60fd005821f1a
yurri2506 <22520590@gm.uit.edu.vn>
2024-12-24 15:58:10 +0700

    config be

--- server.js
+++ server.js
@@ -4,24 +4,11 @@ const bodyParser = require('body-parser');
 const routes = require('./src/routes');
 const cors = require('cors');
 const port = 3000;
-const cors = require('cors');
-
+const app = express();
 app.use(cors({
     origin: ['http://localhost:3001'], // Frontend port
     credentials: true
   }));
-
-const app = express();
-
-// Cấu hình CORS cụ thể
-app.use(cors({
-    origin: (origin, callback) => {
-        // Chấp nhận tất cả origin hoặc yêu cầu không có origin (Postman, cURL)
-        callback(null, origin || '*'); // Trả về origin từ request
-    },
-    credentials: true, // Cho phép gửi cookie hoặc thông tin xác thực
-    optionsSuccessStatus: 200 // Đảm bảo phản hồi thành công cho preflight requests
-}));
 
 // Middleware xử lý JSON
 app.use(bodyParser.json());
