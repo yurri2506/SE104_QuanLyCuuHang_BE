e2d42c7be460186b721cbd38fb912059c767111c
yurri2506 <22520590@gm.uit.edu.vn>
2024-12-28 20:10:02 +0700

    update

--- src/routes/warehouseManageRoute.js
+++ src/routes/warehouseManageRoute.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const warehouseManageController = require('../controllers/warehouseController');
 
-router.get('/get-all', warehouseManageController.getAllReports);
-router.get('/get-report/:thang/:maSanPham', warehouseManageController.getReportByMonthAndProduct);
-router.post('/create', warehouseManageController.createReport);
-router.put('update/:thang/:maSanPham', warehouseManageController.updateReport);
-router.delete('delete/:thang/:maSanPham', warehouseManageController.deleteReport);
-
-module.exports = router;
+// Định tuyến cho báo cáo tồn kho
+router.get('/reports', warehouseManageController.getAllReports); // Lấy tất cả báo cáo
+router.get('/reports/:year/:month', warehouseManageController.generateReport); // Tạo báo cáo tồn kho theo năm và tháng
